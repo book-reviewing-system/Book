9228e66c5dd2fdeb9e7061356aceca15e94b7086
thunguyen251098 <nguyenthithu25101998@gmail.com>
2019-04-03 06:26:38 +0700

    book

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
+# # This file should contain all the record creation needed to seed the database with its default values.
+# # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# #
+# # Examples:
+# #
+# #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+# #   Character.create(name: 'Luke', movie: movies.first)
 (1..15).each do |i|
   User.create(name: "User#{i}", email: "user#{i}@example.com",
     password: "password", password_confirmation: "password")
@@ -41,7 +41,7 @@ sports.each do |sport|
 end
 
 novels.each do |novel|
-  Book.create(category_id: 1, title: novel,
+  Book.create(category_id: 3, title: novel,
     author: "Author",number_of_pages: 300, 
     published_date: DateTime.now.change(year: 2017)
   )
