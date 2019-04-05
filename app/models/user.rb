e488863f6f23eeb8625a0c975b55c678348138ed
ThuNguyen2510 <43926098+ThuNguyen2510@users.noreply.github.com>
2019-04-05 16:20:02 +0700

    dependent: :destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     self.role ||= :standard
   end
   end
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_many :activities
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
